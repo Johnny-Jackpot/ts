54c8a6678e90a403c94b77b23aaae294b33f8cba
oleksandrn <alexandrn@default-value.com>
2024-04-18 23:51:12 +0300

    Leetcode island perimeter

--- leetcode/island-perimeter/island-perimeter.ts
+++ leetcode/island-perimeter/island-perimeter.ts
@@ -1,15 +1,11 @@
 export function islandPerimeter(grid: number[][]): number {
-  const maxRowLength = grid.reduce((prevMax: number, row: number[]) => {
-    return Math.max(prevMax, row.length);
-  }, 0);
-
   let totalLand = 0;
   let totalIntersections = 0;
 
   for (let i = 0; i < grid.length; i++) {
     const row = grid[i];
     const nextRow = grid[i + 1] || [];
-    for (let j = 0; j < maxRowLength; j++) {
+    for (let j = 0; j < row.length; j++) {
       const cell = row[j] || 0;
       const nextHorizontalCell = row[j + 1] || 0;
       const nextVerticalCell = nextRow[j] || 0;
