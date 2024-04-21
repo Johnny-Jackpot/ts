368b1702179d496bd223e44de56f40ed020f2c21
alexandrn <alexandrn@default-value.com>
2024-04-21 21:34:35 +0300

    add two numbers test

--- leetcode/add-two-numbers/add-two-numbers.test.ts
+++ leetcode/add-two-numbers/add-two-numbers.test.ts
@@ -1,28 +1,73 @@
 import {describe, expect, test} from '@jest/globals';
 import {ListNode, addTwoNumbers} from "./add-two-numbers";
 
-describe('Add two numbers', () => {
-  const l1Arr = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
-  const l2Arr = [5, 4, 6];
-  const expectedArr = [6, 4, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
-
-  function getListNodeFromArr(arr: number[]): ListNode {
-    const root = new ListNode();
-    let nextNode = root;
-    arr.forEach((n) => {
-      nextNode.val = n;
+function getListNodeFromArr(arr: number[]): ListNode {
+  const root = new ListNode();
+  let nextNode = root;
+  arr.forEach((n, index) => {
+    nextNode.val = n;
+    if (index !== arr.length - 1) {
       nextNode.next = new ListNode();
       nextNode = nextNode.next;
+    }
+  });
+
+  return root;
+}
+
+describe("getListNodeFromArr", () => {
+  test("getListNodeFromArr returns correct list node", () => {
+    expect(getListNodeFromArr([1,0,0,0,9,8,5]))
+      .toStrictEqual(new ListNode(
+        1,
+        new ListNode(
+          0,
+          new ListNode(
+            0,
+            new ListNode(
+              0,
+              new ListNode(
+                9,
+                new ListNode(
+                  8,
+                  new ListNode(5)
+                )
+              )
+            )
+          )
+        )
+      ));
+  });
+});
+
+type TestDataItem = {
+  title: string,
+  l1: ListNode,
+  l2: ListNode,
+  expected: ListNode,
+}
+const data: TestDataItem[] = [
+  {
+    title: 'Calculate huge number',
+    l1: getListNodeFromArr(
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    ),
+    l2: getListNodeFromArr([5, 4, 6]),
+    expected: getListNodeFromArr(
+      [6, 4, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    ),
+  },
+  {
+    title: 'Calculate with carry',
+    l1: getListNodeFromArr([9,9,9,9,9,9,9]),
+    l2: getListNodeFromArr([9,9,9,9]),
+    expected: getListNodeFromArr([8,9,9,9,0,0,0,1]),
+  },
+];
+describe('Add two numbers', () => {
+  data.forEach((item) => {
+    test(item.title, () => {
+      expect(addTwoNumbers(item.l1, item.l2)).toStrictEqual(item.expected);
     });
-
-    return root;
-  }
-
-  const l1 = getListNodeFromArr(l1Arr);
-  const l2 = getListNodeFromArr(l2Arr);
-  const expected = getListNodeFromArr(expectedArr);
-
-  test('Calculate huge number', () => {
-    expect(addTwoNumbers(l1, l2)).toStrictEqual(expected);
   });
 });
