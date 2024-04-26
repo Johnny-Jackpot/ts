9f728125c00da889fdc8945a368f780a69224498
oleksandrn <alexandrn@default-value.com>
2024-04-26 11:36:07 +0300

    Enum

---
+++ basics/enum/enum.ts
@@ -0,0 +1,23 @@
+export enum EDirection {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
+
+export const ODirection = {
+  UP: 0,
+  DOWN: 1,
+  LEFT: 2,
+  RIGHT: 3,
+} as const;
+
+export function walk(direction: EDirection): EDirection {
+  return direction;
+}
+
+export type Direction = typeof ODirection[keyof typeof ODirection];
+
+export function run(direction: Direction): Direction {
+  return direction;
+}
