61c93181e3c0c22856d2c4c030bcf691eb7a2faf
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-03 00:35:43 +0300

    added basics script

---
+++ basics/index.ts
@@ -0,0 +1,15 @@
+//Run: npx ts-node basics
+
+type TypeUser = {
+    name: string
+    age: number
+    address: string
+}
+
+let user: TypeUser = {
+    name: 'John Rambo',
+    age: 25,
+    address: 'LA'
+}
+
+
