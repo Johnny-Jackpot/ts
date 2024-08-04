4a088f960548982d057a3bdc31157f7aa5009d3a
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-04 22:19:00 +0300

    array

---
+++ basics/arrays.ts
@@ -0,0 +1,12 @@
+let array: string[]
+
+array = ['foo', 'bar']
+
+let anotherArray: Array<number>
+
+anotherArray = [1, 2, 3]
+
+const numbers: ReadonlyArray<number> = [1, 2, 3]
+//numbers.push(7)
+//numbers[3] = 5
+//numbers[0] = 2
