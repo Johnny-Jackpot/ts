044b7a5dec1cb328e862d8f90f1e361faf7e8a29
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-04 22:21:25 +0300

    tuple

--- basics/arrays.ts
+++ basics/arrays.ts
@@ -10,3 +10,6 @@ const numbers: ReadonlyArray<number> = [1, 2, 3]
 //numbers.push(7)
 //numbers[3] = 5
 //numbers[0] = 2
+
+type TypeMyTuple = [number, string, boolean]
+const myTuple: TypeMyTuple = [1, 'foo', true]
