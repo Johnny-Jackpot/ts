cb09f7fd73fc8a5148bad5bc84b0fa6d4c19a1cb
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-06 21:04:49 +0300

    assertions

---
+++ basics/assertions.js
@@ -0,0 +1,8 @@
+var inputElement = document.querySelector('.some-selector');
+var value = inputElement.value;
+var value2 = inputElement.value;
+var getLength = function (text) {
+    return text.length;
+};
+getLength(null);
+getLength('test');
---
+++ basics/assertions.ts
@@ -0,0 +1,10 @@
+const inputElement = document.querySelector('.some-selector')
+const value = (inputElement as HTMLInputElement).value
+const value2 = (<HTMLInputElement>inputElement).value
+
+const getLength = (text: string | null) => {
+    return text!.length
+}
+
+getLength(null)
+getLength('test')
