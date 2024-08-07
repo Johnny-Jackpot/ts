2a8e8ad3a78a2c53bb484ed02bca16aa12077e33
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-07 18:28:15 +0300

    type compatibility

--- basics/type-compatibility.ts
+++ basics/type-compatibility.ts
@@ -6,14 +6,14 @@ interface Point {
 let p1: Point = {x: 10, y: 20}
 let p2: { x: number, y: number } = p1
 
-console.log(p2)
+//console.log(p2)
 
 interface Pet {
     name: string
 }
 
 class Dog {
-    name: string
+    name: string = ''
 }
 
 let pet: Pet
@@ -23,3 +23,17 @@ pet = dog
 
 //error here
 //let anotherDog: Pet = {name: 'Lassie', owner: null}
+
+let x = (a: number) => 0
+let y = (b: number, s: string) => 0
+
+y = x
+//x = y error
+
+let x1 = () => ({name: 'Alice'})
+let y1 = () => ({name: 'Alice', location: 'Seattle'})
+
+x1 = y1
+//y1 = x1 erorr
+
+
