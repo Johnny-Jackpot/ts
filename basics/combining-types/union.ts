2fc8b43cdcab3894c02b450a6b374a7170933103
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-07 19:07:58 +0300

    union

---
+++ basics/combining-types/union.ts
@@ -0,0 +1,12 @@
+function printId(id: number | string) {
+    console.log(id)
+}
+
+printId(1)
+printId('523434')
+//printId({id: 123})
+
+function printId2(id: number | string) {
+    //error
+    //console.log(id.toUpperCase())
+}
