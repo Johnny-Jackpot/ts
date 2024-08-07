8ffb6bf8b982b4a974c8f63b0cd3e6cbfcbe7ed7
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-07 19:13:46 +0300

    intersection

---
+++ basics/combining-types/intersection.ts
@@ -0,0 +1,37 @@
+interface IIdentity {
+    id: number
+    name: string
+}
+
+interface IBusinessPartner {
+    name: string
+    credit: number
+}
+
+interface IContact {
+    email: string
+    phone: string
+}
+
+type TypeEmployee = IIdentity & IContact
+type TypeCustomer = IIdentity & IBusinessPartner
+
+interface IEmployee extends IIdentity, IContact {
+}
+
+interface ICustomer extends IIdentity, IBusinessPartner {
+}
+
+let e: TypeEmployee = {
+    email: "", id: 0, name: "", phone: ""
+}
+
+let e1: IEmployee = {
+    email: "", id: 0, name: "", phone: ""
+}
+
+let c: TypeCustomer = {
+    credit: 0, id: 0, name: ""
+}
+
+let c1: ICustomer = {credit: 0, id: 0, name: ""}
