f33421577c232c1be4e754f50898c7918c396577
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-07 20:29:28 +0300

    constructor overload

---
+++ basics/constructor-overload.ts
@@ -0,0 +1,16 @@
+class Point {
+    protected x: number;
+    protected y: number;
+    constructor(x: number, y: number);
+    constructor(s: string);
+    constructor(xs: any, y?: any) {
+        if (typeof xs === 'string') {
+            this.x = +xs
+            this.y = +xs
+        }
+
+        if (y) {
+            this.y = +y
+        }
+    }
+}
