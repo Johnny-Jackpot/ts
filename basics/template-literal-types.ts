4cb7cb42cd24a240ddf5c13e372004dc53e56689
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-08 22:17:18 +0300

    template literal types

---
+++ basics/template-literal-types.ts
@@ -0,0 +1,11 @@
+type EmailLocaleIds = 'welcome_email' | 'email_heading'
+type FooterLocalIds = 'footer_title' | 'footer_sendoff'
+
+type AllLocalIds = `${EmailLocaleIds | FooterLocalIds}_id`
+
+type AllIds = Record<AllLocalIds, string>
+type AllUpperCaseIds = Record<Uppercase<AllLocalIds>, string>
+type AllCapitalizeCaseIds = Record<Capitalize<AllLocalIds>, string>
+
+
+
